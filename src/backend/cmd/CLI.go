5ff3539579b998ee9db60fbbc25bfeae9f39e20d
Stef <romafon678@gmail.com>
2026-04-10 14:21:12 +0300

    refactor: Cleaned up the code and wrote the input functions

--- src/backend/cmd/CLI.go
+++ src/backend/cmd/CLI.go
@@ -1,64 +1,52 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 	"urbis/src/backend/internal/model"
 	"urbis/src/backend/utilities"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	title, _ := utilities.ReadInput("Введите заголовок:")
+	if title == "" {
+		log.Println("ERROR_READ_EMPTY")
+		return //nil
+	}
 
-	fmt.Println("Введите заголовок:")
-	title, _ := reader.ReadString('\n')
-	title = strings.TrimSpace(title)
-
-	fmt.Println("Введите уровень 1 - 3:")
-	lvl, _ := reader.ReadString('\n')
-	lvl = strings.TrimSpace(lvl)
-	lvlInt, err := strconv.Atoi(lvl)
+	lvl, err := utilities.ReadInt("Введите уровень 1-3:")
 	if err != nil {
-		
+		log.Println("ERROR_READ_INT")
+		return //nil
 	}
 	
-	fmt.Println("Введите долготу (0.000):")
-	lng, _ := reader.ReadString('\n')
-	lng = strings.TrimSpace(lng)
-	lngFloat, err := strconv.ParseFloat(lng, 64)
+	lng, err := utilities.ReadFloat("Введите долготу:")
 	if err != nil {
-		log.Printf("ERROR_PARSING_FLOAT")
-		return
+		log.Println("ERROR_READ_FLOAT")
+		return //nil
 	}
 
-	fmt.Println("Введите широту (0.000):")
-	lat, _ := reader.ReadString('\n')
-	lat = strings.TrimSpace(lat)
-	latFloat, err := strconv.ParseFloat(lat, 64)
+	lat, err := utilities.ReadFloat("Введите широту:")
 	if err != nil {
-		log.Printf("ERROR_PARSING_FLOAT")
-		return
+		log.Println("ERROR_READ_FLOAT")
+		return //nil
 	}
 
 	inc, err := model.CreateIncidentRequest(
 		title,
-		lvlInt,
-		latFloat,
-		lngFloat,
+		lvl,
+		lat,
+		lng,
 	)
 	if err != nil {
 		log.Printf("ERROR_CREATE_INCIDENT")
-		return
+		return //nil
 	}
 
 	payload := utilities.ParseJSON(*inc)
 	if payload == nil {
 		log.Printf("ERROR_GET_JSON")
-		return
+		return //nil
 	}
 
 	fmt.Println(string(payload))

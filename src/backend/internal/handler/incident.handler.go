6664d7da2b94e009e19bd53c964f4b2fad63d348
Stef <romafon678@gmail.com>
2026-04-13 13:53:26 +0300

    feat: - New package validate, service, model - rename files - Create handler create incident

--- src/backend/internal/handler/incident.handler.go
+++ src/backend/internal/handler/incident.handler.go
@@ -1,10 +1,25 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"urbis/src/backend/internal/model"
+	"urbis/src/backend/internal/service/validate"
 )
 
-func  HandlerCreateIncident(w http.ResponseWriter, r *http.Request) {
-	
+func HandlerCreateIncident(w http.ResponseWriter, r *http.Request) {
+	var req model.IncidentRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("invalid request: %v", err), http.StatusInternalServerError)
+	}
+
+	err = validate.Incident(&req)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("invalid struct: %v", err), http.StatusInternalServerError)
+	}
+
+	fmt.Fprintln(w, "Everything is fine!")
 }
---
+++ src/backend/internal/service/validate/incident.validate.go
@@ -0,0 +1,26 @@
+package validate
+
+import (
+	"errors"
+	"urbis/src/backend/internal/model"
+)
+
+func Incident(i *model.IncidentRequest) error {
+	if i.Title == "" {
+		return errors.New("ERROR_TITLE_EMPTY")
+	}
+
+	if i.Lvl < 1 || i.Lvl > 3 {
+		return errors.New("ERROR_LVL_IVALID")
+	}
+
+	if i.Lat < -90.0 || i.Lat > 90.0 {
+		return errors.New("ERROR_LAT_INVALID")
+	}
+
+	if i.Lng < -180.0 || i.Lng > 180.0 {
+		return errors.New("ERROR_LNG_INVALID")
+	}
+
+	return nil
+}
